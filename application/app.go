04012dbea54b621c42c57fd822aa4ebba2b30355
leul <leulmic18@gmail.com>
2024-07-28 21:47:13 +0300

    setup connection to db

--- application/app.go
+++ application/app.go
@@ -4,21 +4,30 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"gorm.io/gorm"
 )
 
 type App struct {
 	router http.Handler
-	db     *gorm.DB
+	DB     *gorm.DB
 }
 
-func New() *App {
+func New() (*App, error) {
 	app := &App{}
+
+	err := app.connectToDB()
+	if err != nil {
+		return nil, fmt.Errorf("error: %w", err)
+	}
+
+	// // making migrations
+	// app.DB.AutoMigrate(&model.User{})
 
 	app.loadRoutes()
 
-	return app
+	return app, nil
 }
 
 func (a *App) Start(ctx context.Context) error {
@@ -27,10 +36,24 @@ func (a *App) Start(ctx context.Context) error {
 		Handler: a.router,
 	}
 
-	err := server.ListenAndServe()
-	if err != nil {
-		return fmt.Errorf("failed to start server: %w", err)
-	}
+	ch := make(chan error, 1) // buffered channel to avoid blocking background context
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to start server: %w", err)
+		}
+		close(ch)
+	}()
 
+	select {
+	case err := <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+		if err := server.Shutdown(timeout); err != nil {
+			return fmt.Errorf("server shutdown failed: %w", err)
+		}
+	}
 	return nil
 }
--- main.go
+++ main.go
@@ -7,15 +7,23 @@ import (
 	"os/signal"
 
 	"github.com/Leul-Michael/go-auth/application"
+	"github.com/joho/godotenv"
 )
 
+func init() {
+	godotenv.Load()
+}
+
 func main() {
-	app := application.New()
+	app, err := application.New()
+	if err != nil {
+		log.Fatal("failed to initialize the app: %w", err)
+	}
 
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
 
-	err := app.Start(ctx)
+	err = app.Start(ctx)
 
 	if err != nil {
 		log.Fatal(err)
